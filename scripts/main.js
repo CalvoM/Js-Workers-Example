97a35a249a9b0afe26ec6bea64d1949a5f035a39
CalvoM <mwadimemakokha@ieee.org>
2020-01-22 22:25:58 +0300

    Style some elements

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ let messageObj={'title':'','message':''}
 let form = document.querySelector('form')
 let searchInput = document.querySelector('input#searchInput')
 let postList = document.querySelector('ul#postList')
-let textSpan = document.createElement('span')
+let textSpan = document.createElement('p')
 if (window.Worker){
     fetchWorker = new Worker('./scripts/fetchWorker.js')
 }
@@ -28,9 +28,11 @@ fetchWorker.onmessage= function(event){
 function updateListUi(data){
     let list_item = document.createElement('li')
     list_item.appendChild(document.createTextNode(data.title))
+    list_item.classList.add('postItem')
     postList.appendChild(list_item)
 }
     if(search_posts.length==0){
         textSpan.textContent = "No posts Available!"
+        textSpan.classList.add('statusNote')
         postList.appendChild(textSpan)
     }
